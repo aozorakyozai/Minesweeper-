30680c2f389df7a9b31c7785acd360977296f955
aozorakyozai <aozorakyozai@gmail.com>
2018-11-24 17:52:29 +0900

    点表示

--- Minesweeper_kai.cpp
+++ Minesweeper_kai.cpp
@@ -1,8 +1,22 @@
-#include<conio.h>
+#include<stdio.h>
+#include<termios.h> // または<conio.h>
 
 #define FIELD_WIDTH 9
 #define FIELD_HEIGHT 9
+int CursolX;
+int CursolY;
 
 int main() {
+	for (int y = 0; y < FIELD_HEIGHT; y++) {
+		for (int x = 0; x < FIELD_WIDTH; x++) {
+			if ((x == CursolX) && (y == CursolY)) {
+				printf("◉");
+			} else {
+				printf("・");
+			}
+		}
+		printf("\n");
+	}
+	_getch();
         
 }
